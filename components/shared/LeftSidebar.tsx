62a2b3aaffab90fdd736cf60857814ef886b1f71
pabitra-kumar <pabitrakumarbebartta@gmail.com>
2023-11-10 11:33:11 +0530

    Bottombar fixed

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,45 @@
+"use client"
+
+import {sidebarLinks} from '@/constants/'
+import Link from 'next/link'
+import Image from 'next/image'
+import { usePathname , useRouter } from 'next/dist/client/components/navigation';
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import logout from "../../public/assets/logout.svg";
+
 function LeftSideBar() {
-    return <h1>LeftSideBar</h1>
+    const router = useRouter();
+    const pathname = usePathname();
+    return (
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex flex-col gap-6 px-6 w-full flex-1">
+                {sidebarLinks.map((link) => 
+                    {
+                        const isActive = (pathname.includes(link.route) && link.route.length > 1) || (pathname === link.route);
+                        return(
+                            <Link href={link.route} key={link.label} className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}>
+                                <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+
+                                <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+                            </Link>
+                        )
+                    })
+                }
+            </div>
+            <div className='mt-10 px-6'>
+                <SignedIn>
+                    <SignOutButton signOutCallback={() => router.push('/sign-in')}>
+                            <div className="flex cursor-pointer gap-4 p-4">
+                                <Image src={logout} alt="logout" width={24} height={24}/>
+                                <p className='text-light-1 max-lg:hidden'>
+                                    Logout
+                                </p>
+                            </div>
+                    </SignOutButton>    
+                </SignedIn>
+            </div>
+        </section>
+    )
 };
 
 export default LeftSideBar;
